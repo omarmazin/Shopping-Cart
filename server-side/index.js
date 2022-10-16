bd2b950e8c7019d820d2cb8c8e044221eb1d3255
omarmazin <omr.ad@outlook.com>
2022-10-16 03:35:46 -0700

    Working on server side

--- server-side/index.js
+++ server-side/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const Products = require('./Products.json')
 app.use(cors)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.post('/products',(req,res)=>{
+  res.send(Products)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  
 })
